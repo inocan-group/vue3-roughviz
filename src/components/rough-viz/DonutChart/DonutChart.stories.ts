0a4dde30903b69c446d29cd8dfbbe3ef472bdec8
Husam Ibrahim <huielbashir@gmail.com>
2020-02-27 15:27:00 +0200

    rough-viz: first pass at donut chart

---
+++ src/components/rough-viz/DonutChart/DonutChart.stories.ts
@@ -0,0 +1,8 @@
+import DonutChart from './DonutChart.vue'
+
+export default { title: 'rough-viz/DonutChart' }
+
+export const donutChart = () => ({
+  components: { DonutChart },
+  template: '<donut-chart/>',
+})
---
+++ src/components/rough-viz/DonutChart/DonutChart.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="chartdiv"></div>
+</template>
+
+<script lang="ts">
+import { defineComponent, onMounted } from '@vue/composition-api'
+import { Donut } from 'rough-viz'
+
+export default defineComponent({
+  setup() {
+    onMounted(() => {
+      new Donut({
+        element: '.chartdiv',
+        legend: false,
+        data: {
+          labels: ['JNCO Jeans', 'Sweat Pants', 'Jorts'],
+          values: [20, 10, 2],
+        },
+        title: 'Pants I Got Clowned On For Wearing In High School',
+        titleFontSize: '2rem',
+        labels: 'letter',
+        values: 'frequency',
+        width: window.innerWidth,
+        stroke: 'coral',
+        color: 'pink',
+        fillWeight: 1.5,
+      })
+    })
+  },
+})
+</script>
