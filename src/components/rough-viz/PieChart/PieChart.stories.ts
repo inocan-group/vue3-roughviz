882682212bab8c24e85981dc748bbc98456331e4
Husam Ibrahim <huielbashir@gmail.com>
2020-02-27 15:24:36 +0200

    rough-viz: first pass at pie chart

---
+++ src/components/rough-viz/PieChart/PieChart.stories.ts
@@ -0,0 +1,8 @@
+import PieChart from './PieChart.vue'
+
+export default { title: 'rough-viz/PieChart' }
+
+export const pieChart = () => ({
+  components: { PieChart },
+  template: '<pie-chart/>',
+})
---
+++ src/components/rough-viz/PieChart/PieChart.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="chartdiv"></div>
+</template>
+
+<script lang="ts">
+import { defineComponent, onMounted } from '@vue/composition-api'
+import { Pie } from 'rough-viz'
+
+export default defineComponent({
+  setup() {
+    onMounted(() => {
+      new Pie({
+        element: '.chartdiv',
+        title: 'Was it useful?',
+        titleFontSize: '1.5rem',
+        legend: false,
+        margin: { top: 50, bottom: 100, left: 40, right: 100 },
+        data: {
+          labels: ['useful', 'no', 'lol idk man'],
+          values: [2, 8, 4],
+        },
+        strokeWidth: 3,
+        fillStyle: 'zigzag-line',
+        highlight: 'gold',
+      })
+    })
+  },
+})
+</script>
