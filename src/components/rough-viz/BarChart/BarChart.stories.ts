01cbf1216dc278a69560ba71b5db468e01126a0f
Husam Ibrahim <huielbashir@gmail.com>
2020-02-27 15:27:23 +0200

    rough-viz: first pass at bar chart

---
+++ src/components/rough-viz/BarChart/BarChart.stories.ts
@@ -0,0 +1,8 @@
+import BarChart from './BarChart.vue'
+
+export default { title: 'rough-viz/BarChart' }
+
+export const barChart = () => ({
+  components: { BarChart },
+  template: '<bar-chart/>',
+})
