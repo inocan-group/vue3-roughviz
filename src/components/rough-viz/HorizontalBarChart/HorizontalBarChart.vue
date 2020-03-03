1192761bb89afa676a9cf54cc9423fd305de0b6a
Husam Ibrahim <huielbashir@gmail.com>
2020-03-04 01:02:13 +0200

    rough-viz: add horizontal bar chart

---
+++ src/components/rough-viz/HorizontalBarChart/HorizontalBarChart.vue
@@ -0,0 +1,36 @@
+<template>
+  <div ref="chartdiv"></div>
+</template>
+
+<script lang="ts">
+import { defineComponent, onMounted, ref, watch } from '@vue/composition-api'
+import { BarH } from 'rough-viz'
+import { useCommonChartOptions, useCommonBarChartOptions } from '@/composables/rough-viz'
+
+export default defineComponent({
+  props: {
+    ...useCommonChartOptions(),
+    ...useCommonBarChartOptions(),
+  },
+  setup(props) {
+    const chartdiv = ref(null)
+    const uid = 'chartdiv' + Date.now()
+
+    onMounted(() => {
+      const el = (chartdiv.value as unknown) as HTMLElement
+      el.id = uid
+
+      watch(() => {
+        el.innerHTML = ''
+
+        new BarH({
+          element: `#${el.id}`,
+          ...props,
+        })
+      })
+    })
+
+    return { chartdiv }
+  },
+})
+</script>
