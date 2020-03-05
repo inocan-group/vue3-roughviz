91789240623125fd7506463f28f745b460a8f18f
Husam Ibrahim <huielbashir@gmail.com>
2020-03-05 23:49:50 +0200

    add basic chart component snippet

---
+++ .vscode/vetur/snippets/template/chart-component.vue
@@ -0,0 +1,23 @@
+<template>
+  <div ref="chartdiv"></div>
+</template>
+
+<script lang="ts">
+import { defineComponent, onMounted, onBeforeUnmount, ref, watch } from '@vue/composition-api'
+
+export default defineComponent({
+  setup() {
+    const chartdiv = ref(null)
+
+    onMounted(() => {
+     
+    })
+
+    onBeforeUnmount(() => {
+     
+    })
+
+    return { chartdiv }
+  },
+})
+</script>
