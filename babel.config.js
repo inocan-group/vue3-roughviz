8c1ff476dd8c85d509e7a075d28f2b9d8a140262
Ken Snyder <ken@ken.net>
2020-09-28 22:00:09 -0700

    build using build script to CJS and ES

--- babel.config.js
+++
@@ -1,5 +0,0 @@
-const devPresets = ['@vue/cli-plugin-babel/preset']
-const buildPresets = ['@babel/preset-env', '@babel/preset-typescript']
-module.exports = {
-  presets: process.env.NODE_ENV === 'development' ? devPresets : buildPresets,
-}
--- dev/serve.ts
+++
@@ -1,4 +0,0 @@
-import { createApp } from 'vue'
-import Dev from './serve.vue'
-
-createApp(Dev).mount('#app')
