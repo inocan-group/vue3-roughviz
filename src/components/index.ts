572e5d66007161cdded1df2f59466e2230750283
Husam Ibrahim <huielbashir@gmail.com>
2020-11-17 16:02:13 +0200

    chore: rename horizontal bar component

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,6 @@
 export { default as Bar } from './Bar.vue';
 export { default as Donut } from './Donut.vue';
-export { default as HorizontalBar } from './HorizontalBar.vue';
+export { default as BarH } from './BarH.vue';
 export { default as Line } from './Line.vue';
 export { default as Pie } from './Pie.vue';
 export { default as Scatter } from './Scatter.vue';
