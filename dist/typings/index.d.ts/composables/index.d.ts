ab991a503dc158277d39d07a7f943a31d1212a46
Husam Ibrahim <huielbashir@gmail.com>
2020-11-18 22:13:05 +0200

    chore: ignore dist folder

--- dist/typings/index.d.ts/composables/index.d.ts
+++
@@ -1,7 +0,0 @@
-import { Ref } from 'vue';
-import { IChartConfig, IChart } from '../@types';
-interface IChartConstructor<T extends IChart> {
-    new (config: IChartConfig<T>): any;
-}
-export declare const useSetupRoughVizChart: <T extends IChart>(Ctor: IChartConstructor<T>, opts: T) => Ref<HTMLElement | null>;
-export {};
