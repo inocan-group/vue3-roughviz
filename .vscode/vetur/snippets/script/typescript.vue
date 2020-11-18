3bae439267b705f88281ae4d67648db591f70b67
Husam Ibrahim <huielbashir@gmail.com>
2020-11-18 22:14:46 +0200

    chore: remove stale files

--- .vscode/vetur/snippets/script/typescript.vue
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-import { Watch, Component, Vue, Prop } from 'vue-property-decorator'
-import { namespace } from 'vuex-class'
-
-@Component
-export default class ${1:ClassName} extends Vue {
-  //
-}
-</script>
