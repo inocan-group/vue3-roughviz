485c28b893e88f2179cae0ea091a76638c7d45d4
Husam Ibrahim <huielbashir@gmail.com>
2020-11-20 03:47:51 +0200

    types: crude type definitions for rough-viz

---
+++ src/shims-augment-rough-viz.d.ts
@@ -0,0 +1,25 @@
+import { IChartConfig, IBar, IPie, ILine, IScatter, IStackedBar } from './@types/index';
+
+declare module '@inocan/rough-viz' {
+  export class Bar {
+    constructor(config: IChartConfig<IBar>);
+  }
+  export class BarH {
+    constructor(config: IChartConfig<IBar>);
+  }
+  export class Donut {
+    constructor(config: IChartConfig<IPie>);
+  }
+  export class Line {
+    constructor(config: IChartConfig<ILine>);
+  }
+  export class Pie {
+    constructor(config: IChartConfig<IPie>);
+  }
+  export class Scatter {
+    constructor(config: IChartConfig<IScatter>);
+  }
+  export class StackedBar {
+    constructor(config: IChartConfig<IStackedBar>);
+  }
+}
---
+++ src/shims-declare-rough-viz.d.ts
@@ -0,0 +1,1 @@
+declare module '@inocan/rough-viz';
